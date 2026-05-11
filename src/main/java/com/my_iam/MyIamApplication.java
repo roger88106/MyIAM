6bc51fe1e7ca6127a61e0feaab0b6b6d81bc60a1
roger <roger88106@gmail.com>
2026-05-11 21:30:55 +0900

    PJ立上げ

---
+++ src/main/java/com/my_iam/MyIamApplication.java
@@ -0,0 +1,13 @@
+package com.my_iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyIamApplication.class, args);
+	}
+
+}
